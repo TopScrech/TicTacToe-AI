048fe75b43619934102c1034ea6f394be9e62df5
TopScrech <sergei_saliukov@icloud.com>
2025-09-18 10:55:55 +0200

    Initial Commit

---
+++ TicTacToe-Test/TicTacToe_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TestApp.swift
+//  TicTacToe-Test
+//
+//  Created by Sergei Saliukov on 18.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

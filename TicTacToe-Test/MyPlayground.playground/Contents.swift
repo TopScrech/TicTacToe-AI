5274dffff755c46c8aec2b3a4b1b9ba7f48579e2
TopScrech <sergei_saliukov@icloud.com>
2025-09-18 11:29:03 +0200

    isWin func

--- TicTacToe-Test/MyPlayground.playground/Contents.swift
+++ TicTacToe-Test/MyPlayground.playground/Contents.swift
@@ -1,84 +1,46 @@
 import Foundation
 
-var isWin = false
-var board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
-printBoard(board)
-
-moveRandom("x")
-printBoard(board)
-
-moveRandom("o")
-printBoard(board)
-
-moveRandom("x")
-printBoard(board)
-
-moveRandom("o")
-printBoard(board)
-
-moveRandom("x")
-printBoard(board)
-
-moveRandom("o")
-printBoard(board)
-
-moveRandom("x")
-printBoard(board)
-
-moveRandom("o")
-printBoard(board)
-
-moveRandom("x")
-printBoard(board)
+var board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+var gameOver = false
 
 func printBoard(_ board: [[String]]) {
-    print("\(board[0])\n\(board[1])\n\(board[2])")
+    print("\n\(board[0])\n\(board[1])\n\(board[2])\n")
+}
+
+@MainActor
+func isFull() -> Bool {
+    !board.flatMap { $0 }.contains(" ")
+}
+
+@MainActor
+func hasWin(_ move: String) -> Bool {
+    for r in 0..<3 { if board[r][0] == move && board[r][1] == move && board[r][2] == move { return true } }
+    for c in 0..<3 { if board[0][c] == move && board[1][c] == move && board[2][c] == move { return true } }
+    if board[0][0] == move && board[1][1] == move && board[2][2] == move { return true }
+    if board[0][2] == move && board[1][1] == move && board[2][0] == move { return true }
+    return false
 }
 
 @MainActor
 func moveRandom(_ move: String) {
-    guard !isWin(move) else {
-        print("Finished")
-        return
-    }
+    guard !gameOver else { return }
+    guard !isFull() else { gameOver = true; print("Finished"); return }
     
-    let randomRow = Int.random(in: 0...2)
-    let randomColumn = Int.random(in: 0...2)
+    let r = Int.random(in: 0...2)
+    let c = Int.random(in: 0...2)
     
-    if board[randomRow][randomColumn] == " " {
-        board[randomRow][randomColumn] = move
+    if board[r][c] == " " {
+        board[r][c] = move
+        printBoard(board)
+        if hasWin(move) { gameOver = true; print("\(move.uppercased()) wins"); return }
+        if isFull() { gameOver = true; print("Finished"); return }
     } else {
         moveRandom(move)
     }
-    
-    print(isWin(move))
 }
 
-@MainActor
-func isWin(_ move: String) -> Bool {
-    // Rows
-    for row in 0..<3 {
-        if board[row][0] == move && board[row][1] == move && board[row][2] == move {
-            return true
-        }
-    }
-    
-    // Columns
-    for col in 0..<3 {
-        if board[0][col] == move && board[1][col] == move && board[2][col] == move {
-            return true
-        }
-    }
-    
-    // Diagonals
-    if board[0][0] == move && board[1][1] == move && board[2][2] == move {
-        return true
-    }
-    
-    if board[0][2] == move && board[1][1] == move && board[2][0] == move {
-        return true
-    }
-    
-    return false
+var current = "x"
+while !gameOver {
+    moveRandom(current)
+    current = (current == "x") ? "o" : "x"
 }
